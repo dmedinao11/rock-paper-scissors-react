ff185c5ebdcb4369a4c64d13542512fc0a9f6968
Daniel Medina <danielmedina1119@gmail.com>
2021-03-15 08:53:13 -0500

    Animate css añadido

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import "animate.css";
 
 import { Score } from "./components/score/Score";
 import Game from "./components/game/Game";
